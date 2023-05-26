728df61506b245429ba4b79c7a281afb5344e45e
severine-cuenot <nine.injections@gmail.com>
2023-05-26 13:03:22 +0200

    all ressources are in place!

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,13 @@
 // == Import
-import reactLogo from './react-logo.svg';
+// import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
+      {/* <img src={reactLogo} alt="react logo" /> */}
+      <h1>Hello World_</h1>
     </div>
   );
 }
