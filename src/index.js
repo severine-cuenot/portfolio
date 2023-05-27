db16070ff007c72efccf611e11de6409a0c59631
severine-cuenot <nine.injections@gmail.com>
2023-05-27 21:29:12 +0200

    :wrench: config : all files are in place and working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'src/components/App';
-import store from 'src/Store';
+// import store from 'src/Store';
 
 const rootReactElement = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  // <Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </Provider>
 );
 
 const root = createRoot(document.getElementById('root'));
