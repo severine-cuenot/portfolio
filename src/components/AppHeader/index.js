6d973a8288fa70085087f22d14e69f0dea8ddc76
severine-cuenot <nine.injections@gmail.com>
2023-05-29 18:37:11 +0200

    :rocket: feat : started accessible burger menu

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,11 +1,32 @@
-// import { Link } from 'react-router-dom';
+/* eslint-disable react/button-has-type */
+
+import { Link } from 'react-router-dom';
+
 import './style.scss';
 
 function AppHeader() {
   return (
-    <p className="footer__desc">
-      Monté à la sueur de mon front avec React.
-    </p>
+    <nav id="main-nav" role="navigation" aria-label="Menu de navigation">
+      {/* <div>My Logo Here</div> */}
+      {/* accessible burger menu */}
+      <button
+        id="main-nav__burger-button"
+        aria-expanded="false"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          {/* Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+        </svg>
+        <span>Menu</span>
+      </button>
+      <ul className="main-nav__menu visible">
+        <li><Link to="/parcours" className="">Parcours</Link></li>
+        <li><Link to="/experience" className="">Expérience</Link></li>
+        <li><Link to="/realisations" className="">Réalisations</Link></li>
+        <li><Link to="/contact" className="">Contact</Link></li>
+        <li>Télécharger mon CV BTN</li>
+      </ul>
+    </nav>
   );
 }
 
