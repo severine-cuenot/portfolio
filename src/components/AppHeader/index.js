6b3c511d079cb6bc396d1b15451e6310685e2971
severine-cuenot <nine.injections@gmail.com>
2023-05-31 19:08:30 +0200

    :art: added some scss variables and adjusting burger menu

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -23,7 +23,7 @@ function AppHeader() {
           {/* Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
           <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
         </svg>
-        <span>Menu</span>
+        <span className="main-nav__burger-button--text">Menu</span>
       </button>
       <ul className={isExpanded ? 'main-nav__menu-expanded' : 'main-nav__menu-not-expanded'}>
         <li><Link to="/parcours" className="">Parcours</Link></li>
