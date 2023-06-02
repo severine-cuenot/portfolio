f0d7ca058bbe63b5c8b1d09ad353a5d0e2dd5daf
severine-cuenot <nine.injections@gmail.com>
2023-06-02 22:12:27 +0200

    :rocket: feat : started wording and some CSS

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,10 +8,27 @@ import Block from 'src/components/Block';
 function Home() {
   return (
     <Page>
-      <Block>
-        <h1>
-          Hello World
+      <Block className="block__left">
+        <h1 className="home__welcome--title">
+          Hello World_
         </h1>
+        <h2 className="home__name">
+          Séverine Cuenot<br />
+          <span>Développeuse Web</span>
+        </h2>
+        <p>
+          Illustratrice de formation, j’ai découvert par hasard que le CSS,
+          c’était trop bien. Du coup, j’ai fait une formation intensive de 6&nbsp;mois
+          en Fullstack JS avec spécialisation en Front & React.js.
+        </p>
+        <p>
+          En reconversion professisonnelle,
+          je cherche désormais à consolider cette certitude que le Front,
+          c’est encore mieux que le CSS tout seul.
+        </p>
+      </Block>
+      <Block className="block__right">
+        img
       </Block>
     </Page>
   );
