bdb558047c450a064a3f3448a968aab92ea3c427
Nine Poppy <nine.injections@gmail.com>
2023-06-04 10:59:10 +0200

    :art: button style done

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -27,7 +27,7 @@ function Home() {
             je cherche désormais à consolider cette certitude que le Front,
             c’est encore mieux que le CSS tout seul.
           </p>
-          <Button label="En savoir plus sur mon expérience_" type="button" />
+          <Button label="En savoir plus_" type="button" />
         </Block>
         <Block>
           <img src="../../img/nine_liberty.png" alt="Nine" className="home__img" />
