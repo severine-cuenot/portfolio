65657e45536723f2342cbe676d00f7803730c7e0
Nine Poppy <nine.injections@gmail.com>
2023-06-04 11:44:07 +0200

    :art: adjust CSS for footer and home with media queries

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,29 +9,31 @@ function Home() {
   return (
     <Page>
       <Container>
-        <Block>
-          <h1 className="home__welcome--title">
-            Hello World_
-          </h1>
-          <h2 className="home__name">
-            Séverine Cuenot<br />
-            <span>Développeuse Web</span>
-          </h2>
-          <p className="home__content">
-            Illustratrice de formation, j’ai découvert par hasard que le CSS,
-            c’était trop bien. Du coup, j’ai fait une formation intensive de 6&nbsp;mois
-            en Fullstack JS avec spécialisation en Front & React.js.
-          </p>
-          <p className="home__content">
-            En reconversion professisonnelle,
-            je cherche désormais à consolider cette certitude que le Front,
-            c’est encore mieux que le CSS tout seul.
-          </p>
-          <Button label="En savoir plus_" type="button" />
-        </Block>
-        <Block>
-          <img src="../../img/nine_liberty.png" alt="Nine" className="home__img" />
-        </Block>
+        <section className="main">
+          <Block className="main__block">
+            <h1 className="home__welcome--title">
+              Hello World_
+            </h1>
+            <h2 className="home__name">
+              Séverine Cuenot<br />
+              <span>Développeuse Web</span>
+            </h2>
+            <p className="home__content">
+              Illustratrice de formation, j’ai découvert par hasard que le CSS,
+              c’était trop bien. Du coup, j’ai fait une formation intensive de 6&nbsp;mois
+              en Fullstack JS avec spécialisation en Front & React.js.
+            </p>
+            <p className="home__content">
+              En reconversion professisonnelle,
+              je cherche désormais à consolider cette certitude que le Front,
+              c’est encore mieux que le CSS tout seul.
+            </p>
+            <Button label="En savoir plus_" type="button" />
+          </Block>
+          <Block className="home__img--block">
+            <img src="../../img/nine_liberty.png" alt="Nine" className="home__img" />
+          </Block>
+        </section>
       </Container>
     </Page>
   );
