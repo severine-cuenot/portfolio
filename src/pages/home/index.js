b005dcfe51e6b82290faf800afbf8d885d696dc6
severine-cuenot <nine.injections@gmail.com>
2023-06-10 22:26:29 +0200

    :rocket: feat : added a timer J+2months

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Page from 'src/components/Page';
 // import Block from 'src/components/Block';
 import Container from '../../components/Container';
@@ -6,6 +7,16 @@ import Button from '../../components/Button';
 import './style.scss';
 
 function Home() {
+  const [dateState, setDateState] = useState(new Date());
+  // Add 2 months to current date
+  const months = 2;
+  dateState.setMonth(dateState.getMonth() + months);
+
+  // Refresh DOM every 30 sec
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+
   return (
     <Page>
       <Container>
@@ -18,6 +29,15 @@ function Home() {
               Séverine Cuenot<br />
               <span>Développeuse Web</span>
             </h2>
+            <div className="timer">
+              Disponible à partir du
+              {' '} {/* add a space */}
+              {dateState.toLocaleDateString('fr-FR', {
+                day: 'numeric',
+                month: 'short',
+                year: 'numeric',
+              })}.
+            </div>
             <p className="home__content">
               Illustratrice de formation, j’ai découvert par hasard que le CSS,
               c’était trop bien. Du coup, j’ai fait une formation intensive de 6&nbsp;mois
