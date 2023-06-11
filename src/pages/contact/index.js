62d3e08045ba62e9364db2d1985e37c8856f169e
severine-cuenot <nine.injections@gmail.com>
2023-06-11 13:32:40 +0200

    :rocket: feat : safe email from spam robots

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -3,9 +3,26 @@ import Page from '../../components/Page';
 import './style.scss';
 
 function Contact() {
+  const emailPart1 = 'severine.cuenot.dev';
+  const emailPart2 = 2 ** 6;
+  const emailPart3 = String.fromCharCode(emailPart2);
+  const emailPart4 = 'gmail.com';
+  const emailPart5 = emailPart1 + String.fromCharCode(emailPart2) + emailPart4;
+  const emailLink = `mailto:${emailPart5}`;
+  const emailText = emailPart1 + emailPart3 + emailPart4;
+
   return (
     <Page>
-      <p>test</p>
+      <section>
+        <header>
+          <h1>Contact</h1>
+          <p>
+            Si vous souhaitez me contacter, vous pouvez
+            m'écrire à <a href={emailLink}>{emailText}</a> ou bien remplir le formulaire ci-dessous.
+          </p>
+
+        </header>
+      </section>
     </Page>
   );
 }
