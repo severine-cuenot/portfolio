8176f9b7a369216b5bf1d24435f57afd12280a98
severine-cuenot <nine.injections@gmail.com>
2023-06-13 21:35:39 +0200

    :rocket: feat : testing Emailjs

--- src/middleware/contactForm.js
+++ src/middleware/contactForm.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 import { SEND_MESSAGE, sentMsg } from '../actions';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000', // Utilisez le même port que celui sur lequel votre serveur écoute
+  baseURL: 'https://vercel.com/severine-cuenot/portfolio-backend/rXnVQ7cfTK8B1wehZsaN5SrVqTeZ', // Utilisez le même port que celui sur lequel votre serveur écoute
 });
 
 const contactForm = (store) => (next) => (action) => {
   if (action.type === SEND_MESSAGE) {
-    const { fullname, email, message } = store.getState();
-    instance.post('/api/contact', { fullname, email, message })
+    const { name, email, message } = store.getState();
+    instance.post('/api/contact', { name, email, message })
       .then((response) => {
         store.dispatch(sentMsg(response.data.status));
       })
