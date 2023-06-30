63eb11a53ee6a3e7e748184406132c0181020b47
severine-cuenot <nine.injections@gmail.com>
2023-06-30 21:18:57 +0200

    :rocket: feat: added accordion on experience page. it works. Now needs style and some adjustment

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import experiences from '../../data/experiences';
 import AccordionContent from './accordionContent';
 
@@ -5,10 +6,23 @@ import './style.scss';
 
 function Accordion() {
   // console.log(experiences);
+  const [clicked, setClicked] = useState('0');
+  const handleToggle = (id) => {
+    if (clicked === id) {
+      return setClicked('0');
+    }
+    setClicked(id);
+  };
+
   return (
     <ul>
       {experiences.map((experience) => (
-        <AccordionContent key={experience.id} experience={experience} />
+        <AccordionContent
+          key={experience.id}
+          experience={experience}
+          onToggle={() => handleToggle(experience.id)}
+          active={clicked === experience.id}
+        />
       ))}
     </ul>
   );
