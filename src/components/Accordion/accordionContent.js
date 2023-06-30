5572b488961fb34eb1e622d64f9b93df34c4a471
severine-cuenot <nine.injections@gmail.com>
2023-06-30 21:18:57 +0200

    :rocket: feat: added accordion on experience page. it works. Now needs style and some adjustment

--- src/components/Accordion/accordionContent.js
+++ src/components/Accordion/accordionContent.js
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
 import './style.scss';
 
-function AccordionContent({ experience }) {
+function AccordionContent({ experience, onToggle, active }) {
   const { title, content } = experience;
   // console.log(experience);
   return (
-    <li className="accordion_item">
-      <button className="button" type="button">
+    <li className={`accordion_item ${active ? 'active' : ''}`}>
+      <button className="button" type="button" onClick={onToggle}>
         {title}
-        <span className="control">—</span>
+        <span className="control">{active ? '—' : '+'}</span>
       </button>
-      <div className="answer_wrapper">
+      <div className={`answer_wrapper ${active ? 'open' : ''}`}>
         <div className="answer">{content}</div>
       </div>
     </li>
@@ -18,8 +18,9 @@ function AccordionContent({ experience }) {
 }
 
 AccordionContent.propTypes = {
-  experience: PropTypes.object.isRequired,
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
+  experience: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }).isRequired,
 };
 export default AccordionContent;
--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,15 +1,18 @@
 export default [
   {
+    id: 0,
     title: 'Publications Agora',
     content:
       'Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium. Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.',
   },
   {
+    id: 1,
     title: 'Illustration',
     content:
       'Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque. Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque.',
   },
   {
+    id: 2,
     title: 'Divers',
     content:
       'Blanditiis aliquid adipisci quisquam reiciendis voluptates itaque.',
