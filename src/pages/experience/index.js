5f69cf8a740d7189e1f2564babea6261b5848169
severine-cuenot <nine.injections@gmail.com>
2023-07-01 17:44:10 +0200

    :art: style : styling experiences

--- src/pages/experience/index.js
+++ src/pages/experience/index.js
@@ -1,12 +1,20 @@
 import Page from '../../components/Page';
 import Accordion from '../../components/Accordion';
 
-import './style.scss';
-
 function Experience() {
   return (
     <Page>
-      <Accordion />
+      <section className="section">
+        <header className="section__header">
+          <h1 className="section__title">Expérience</h1>
+        </header>
+        <p>
+          Pour connaître le détail des tâches qui m'ont été confiées
+          au fil des ans, cliquez sur le nom des entreprises pour lesquelles
+          j'ai travaillé.
+        </p>
+        <Accordion />
+      </section>
     </Page>
   );
 }
