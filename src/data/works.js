2ef9db8e3b919b5acda7a6969161931866ebe81f
severine-cuenot <nine.injections@gmail.com>
2023-07-02 19:24:05 +0200

    :wrench: config : creating a JSON for works

---
+++ src/data/works.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 0,
+    title: 'Transmission des Saevoirs',
+    context: 'Ecole O\'clock, passage du titre pro',
+    img: '',
+    text: 'blablabla blibliblbi',
+    tools: 'HTML, Javascript, SCSS, nodemailer, blablabla',
+    link: '',
+  },
+  {
+    id: 1,
+    title: 'The Oxford Communiqué',
+    context: 'Publications Agora',
+    img: '',
+    text: 'blablabla blibliblbi',
+    tools: 'HTML, CSS',
+    link: '',
+  },
+];
