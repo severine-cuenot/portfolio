c17c891860c1a27be178f12b80548498e9da70c5
severine-cuenot <nine.injections@gmail.com>
2023-07-18 14:13:28 +0200

    :art: style : finised style but need desktop adjustment

--- src/components/Works/oneWork.js
+++ src/components/Works/oneWork.js
@@ -11,8 +11,12 @@ function OneWork({ work }) {
       <div className="work__img">
         <img src={`../../img/${img}`} alt={`${title}`} className="work__img--src" />
       </div>
-      <div>
-        {title} {context} {text} {tools} {link}
+      <div className="work__description">
+        <div className="work__title">{title}</div>
+        <div className="work__context">{context}</div>
+        <div className="work__content">{text}</div>
+        <div className="work__tools">{tools}</div>
+        <div className="work__link"><a href={link}>Voir la créa</a></div>
       </div>
     </li>
   );
@@ -24,7 +28,7 @@ OneWork.propTypes = {
     img: PropTypes.string.isRequired,
     context: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
-    tools: PropTypes.string.isRequired,
+    tools: PropTypes.array.isRequired,
     link: PropTypes.string.isRequired,
   }).isRequired,
 };
