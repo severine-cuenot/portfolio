5d925fac3a813ac9cc7fd4b0497f09a748e8a6e3
severine-cuenot <nine.injections@gmail.com>
2023-07-19 12:27:54 +0200

    :art: style : adjusted CSS for about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import Page from '../../components/Page';
 
-import './style.scss';
-
 function About() {
   return (
     <Page>
@@ -12,22 +10,35 @@ function About() {
         <header className="section__header">
           <h1 className="section__title">Parcours</h1>
         </header>
+        <div className="section__box--avatar">
+          <img src="../../img/avatar.png" alt="portrait" className="section__box--avatar-img" />
+        </div>
+        <div className="section__box--text">
+          <p className="section__text">J'ai commencé mon parcours en obtenant un diplôme d'Illustration/bande-dessinée et me suis alors lancée en auto-entreprise.</p>
 
-        <p className="section__text">Ma découverte du milieu des métiers du web s'est effectuée à la manière d'une aventurière luttant contre une jungle dense pour se frayer un chemin jusqu'au précieux et tant espéré trésor.</p>
+          <p className="section__text">Seulement le milieu de l'art n'étant pas le plus rémunérateur, j'ai dû partir en quête d'un emploi alimentaire pour pouvoir vivre plus sereinement.</p>
 
-        <p className="section__text">Un diplôme de Bande-dessinée et d'Illustration en poche, j'ai tout d'abord tenté ma chance en freelance. Le milieu de l'art étant ce qu'il est, il a fallu que je trouve un emploi “alimentaire” en parallèle pour pouvoir payer ces maudites factures.</p>
+          <p className="section__text">M'a alors été donné l'opportunité d'être engagée dans une maison d'édition proposant son contenu en ligne, bien que je n'avais aucune expérience dans ce domaine. J'ai ainsi découvert les métiers d'intégratrice et de secrétaire de rédaction web.</p>
 
-        <p className="section__text">M'a alors été donné l'opportunité d'être engagée dans une maison d'édition proposant son contenu en ligne, bien que je n'avais aucune expérience dans ce domaine. J'ai ainsi découvert les métiers d'intégratrice et de secrétaire de rédaction web.</p>
+          <p className="section__text">On m'a confié des tâches aussi variées que la mise à jour du Wordpress de l'entreprise, l'intégration des newsletters, des mailings publicitaires, des landings d'abonnement, des bons de commande...</p>
 
-        <p className="section__text">On m'a confié des tâches aussi variées que la mise à jour du Wordpress de l'entreprise, l'intégration des newsletters, des mailings publicitaires, des landings d'abonnement, des bons de commande...</p>
+          <p className="section__text">Totalement autodidacte, je me suis plongée avec curiosité dans le HTML et le CSS, deux langages dont je ne connaissais rien et qui m'ont rapidement passionnée.</p>
 
-        <p className="section__text">Totalement autodidacte, je me suis plongée avec curiosité dans le HTML et le CSS, deux langages dont je ne connaissais rien et qui se sont avérés hyper enthousiasmants. Petit à petit, j'ai pris mes marques et ai eu envie d'en apprendre toujours plus. Bidouillant ici un blog, là un Wordpress, je me suis rendue compte que le métier de Développeur Web pouvait être une véritable vocation.</p>
+          <p className="section__text">Petit à petit, j'ai pris mes marques et ai eu envie d'en apprendre toujours plus. Bidouillant ici un blog, là un Wordpress, je me suis rendue compte que le métier de Développeur Web pouvait être une véritable vocation.</p>
 
-        <p className="section__text">Après plus de dix ans à servir mon entreprise, j'ai alors pris la décision de me reconvertir. Armée de ma motivation et muselant cette petite voix terrorisée face à toute idée de changement radical, j'ai fait les démarches auprès de Transition Pro pour obtenir un financement.</p>
+          <p className="section__text">Seulement être autodidacte a ses limites et mon entreprise ne me proposait pas de me former. Après dix ans à la servir, j'ai alors pris la décision de me reconvertir.</p>
 
-        <p className="section__text">Quelques mois plus tard, après une formation aussi intense que passionnante, épanouissante et galvanisante, me voici les yeux brillants d'une envie réelle de me faire une place au soleil... ou en tout cas devant la lumière bleue d'un écran d'ordinateur&nbsp;!</p>
+          <p className="section__text">Grâce à Transition Pro, j'ai eu la chance de pouvoir rejoindre l'Ecole O'Clock en mai 2022, pour une formation Fullstack Javascript.</p>
 
-        <Link to="/experience"><Button label="Expérience_" type="button" /></Link>
+          <p className="section__text">Cette formation très intensive se déroulait sur 5 mois en téléprésentiel&nbsp;:</p>
+          <ul className="section__list">
+            <li>nous avons eu 3 mois de formation socle avec les bases solides du HTML, du CSS, de Javascript, du SQL mais aussi des bonnes pratiques en matière d'organisation de travail, de sécurité ou de développement.</li>
+            <li>ensuite, nous avons eu un mois de spécialisation au choix entre le front et le back. Pour ma part, j'ai choisi de m'orienter vers le Front avec REACT.</li>
+            <li>Enfin, le dernier mois était consacré au développement d'un projet professionnel en équipe.</li>
+          </ul>
+
+          <Link to="/experience"><Button label="Expérience_" type="button" /></Link>
+        </div>
       </section>
     </Page>
   );
