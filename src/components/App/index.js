cf29a3882d14b893d9ae2871d8db04da0d4e6492
severine-cuenot <nine.injections@gmail.com>
2023-08-08 16:20:25 +0200

    hammer: fix: problem react-router-dom vs surge fixed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -41,7 +41,6 @@ function App() {
           <Route path="/realisations" element={<Realisations />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/credits" element={<Credits />} />
-          <Route path="/404" element={<NotFound />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <AppFooter />
